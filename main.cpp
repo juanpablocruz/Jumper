3dd08a9759e2ab0a66216de08e28a0f9e74dbbd3
Juan Pablo <pablo@cruzf.net>
2013-05-01 15:13:47 +0200

    stage2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-=======
 #ifdef _WIN32 
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 #include <allegro5\allegro.h>
 #include <allegro5\allegro_image.h>
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
-<<<<<<< HEAD
-#include <vector>
-#include <string>
-=======
 #else
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
@@ -22,7 +15,6 @@
 #include <vector>
 #include <string>
 #include <math.h>
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 #include "objects.h"
 
 enum Keys{UP, DOWN, LEFT, RIGHT, SPACE};
@@ -30,10 +22,7 @@ bool keys[5] = {false, false, false, false, false};
 ALLEGRO_FONT *font12 = NULL;
 
 Character *hero;
-<<<<<<< HEAD
-=======
 int drawx, drawy;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 
 
 int main(void){
@@ -42,20 +31,13 @@ int main(void){
 	bool redraw = true;
 	bool drawLine = false;
 	const int FPS = 20;
-<<<<<<< HEAD
-
-=======
 	drawy=0;
 	drawx=0;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
-<<<<<<< HEAD
-=======
 	ALLEGRO_BITMAP *ground = NULL;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 
 	if(!al_init())
 		return -1;
@@ -71,18 +53,7 @@ int main(void){
 	al_install_mouse();
 	al_init_font_addon();
 	al_init_ttf_addon();
-<<<<<<< HEAD
 
-	muros.push_back(new Wall(300,HEIGHT-60,36,36,true));
-	resources.push_back(al_load_bitmap("wall.png"));
-	muros[muros.size()-1]->sprite = resources.size()-1;
-	
-	hero = new Character(100,HEIGHT-50,muros);
-	resources.push_back(al_load_bitmap("character.png"));
-	hero->assignResource(resources.size()-1);
-
-=======
-	
 	for (int i = 0; i < 1+(WIDTH/18); i++)
 	{
 		for (int j = 0; j < 1+(HEIGHT/36); j++)
@@ -116,7 +87,6 @@ int main(void){
 
 	ground = al_load_bitmap("ground.png");
 
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 	event_queue = al_create_event_queue();
 	timer = al_create_timer(1.0 / FPS);
 	font12 = al_load_font("arial.ttf", 12, 0);
@@ -133,14 +103,11 @@ int main(void){
 
 		if(ev.type == ALLEGRO_EVENT_TIMER){
 			redraw = true;
-<<<<<<< HEAD
-=======
 			if(!hero->moving){
 				hero->imgy = 0;
 				hero->imgx = 0;
 			}
 
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 			if(keys[LEFT]){
 				hero->medio++;
 				if(hero->medio%6==0){
@@ -148,16 +115,12 @@ int main(void){
 					hero->medio = 0;
 				}
 				hero->imgy = 1;
-<<<<<<< HEAD
-				hero->move(-1);
-=======
 				if((hero->x-WIDTH/2)>-hero->xSpeed){
 					if(drawx>0){
 						drawx--;
 					}
 				}
 				else hero->move(-1);
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 			}
 			if(keys[RIGHT]){
 				hero->medio++;
@@ -166,16 +129,12 @@ int main(void){
 					hero->medio = 0;
 				}
 				hero->imgy = 2;
-<<<<<<< HEAD
-				hero->move(1);
-=======
 				if((hero->x-WIDTH/2)>-hero->xSpeed){
 					if(drawx==13)drawx=0;
 					else
 					drawx++;
 				}
 				else	hero->move(1);
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 			}
 			hero->imgx =(hero->imgx)%(hero->COLS);
 		}
@@ -198,27 +157,18 @@ int main(void){
 				break;
 			case ALLEGRO_KEY_UP:
 				keys[UP] = true;
-<<<<<<< HEAD
-=======
 				hero->moving = true;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 				break;
 			case ALLEGRO_KEY_DOWN:
 				keys[DOWN] = true;
 				break;
 			case ALLEGRO_KEY_LEFT:
 				keys[LEFT] = true;
-<<<<<<< HEAD
-				break;
-			case ALLEGRO_KEY_RIGHT:
-				keys[RIGHT] = true;
-=======
 				hero->moving = true;
 				break;
 			case ALLEGRO_KEY_RIGHT:
 				keys[RIGHT] = true;
 				hero->moving = true;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 				break;
 			case ALLEGRO_KEY_SPACE:
 				hero->numJumps++;
@@ -230,27 +180,18 @@ int main(void){
 			switch(ev.keyboard.keycode){
 			case ALLEGRO_KEY_UP:
 				keys[UP] = false;
-<<<<<<< HEAD
-=======
 				hero->moving = false;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 				break;
 			case ALLEGRO_KEY_DOWN:
 				keys[DOWN] = false;
 				break;
 			case ALLEGRO_KEY_LEFT:
 				keys[LEFT] = false;
-<<<<<<< HEAD
-				break;
-			case ALLEGRO_KEY_RIGHT:
-				keys[RIGHT] = false;
-=======
 				hero->moving = false;
 				break;
 			case ALLEGRO_KEY_RIGHT:
 				keys[RIGHT] = false;
 				hero->moving = false;
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 				break;
 			case ALLEGRO_KEY_SPACE:
 				keys[SPACE] = false;
@@ -259,9 +200,7 @@ int main(void){
 		}
 		if(redraw && al_is_event_queue_empty(event_queue)){
 			redraw = false;
-<<<<<<< HEAD
-=======
-			
+
 			//Draw ground		
 			for (int i = drawy; i < 1+drawy+(WIDTH/18); i++)
 			{
@@ -272,36 +211,21 @@ int main(void){
 
 				}
 			}
-	 
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
+
 			hero->update();
 			for(int i=0;i<muros.size();i++){
 				al_draw_bitmap(resources[muros[i]->sprite],muros[i]->x,muros[i]->y,0);
 			}
 			render(hero,resources);
-<<<<<<< HEAD
-			al_draw_textf(font12, al_map_rgb(255, 255, 255), 205, 5, 0, "Muro x: %.2f Muro y: %.2f",muros[0]->x,muros[0]->y);
-			al_draw_textf(font12, al_map_rgb(255, 255, 255), 205, 20, 0, "Hero x: %.2f Hero y: %.2f",hero->x,hero->y);
-			if(hero->checkCollide())
-				al_draw_textf(font12, al_map_rgb(255, 255, 255), 205, 35, 0, "Choca");
-			else
-				al_draw_textf(font12, al_map_rgb(255, 255, 255), 205, 35, 0, "No Choca");
-			al_flip_display();
-			al_clear_to_color(al_map_rgb(0,0,0));
-=======
 			al_draw_textf(font12, al_map_rgb(255, 255, 255), 205, 5, 0, "herox: %d",Mapa[(int)hero->x/36][(int)hero->y/36]);
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(100,100,250));
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 		}
 	}
 	for(int i=0;i<resources.size();i++){
 		al_destroy_bitmap(resources[i]);
 	};
-<<<<<<< HEAD
-=======
 	al_destroy_bitmap(ground);
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 	al_destroy_event_queue(event_queue);
 	al_destroy_timer(timer);
 	al_destroy_font(font12);
--- objects.h
+++ objects.h
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-#ifndef OBJECTS_H
-#define OBJECTS_H
-
-#include <allegro5\allegro.h>
-#include <allegro5\allegro_image.h>
-#include <vector>
-
-#define PI 3.14159265
-
-using namespace std;
-
-static vector<ALLEGRO_BITMAP*> resources;
-
-static const int WIDTH = 800;
-static const int HEIGHT = 600;
-
-class Wall{
-public:
-	float width,height,x,y,sprite;
-	bool ground,collide;
-
-	Wall(float x,float y, float w, float h, bool ground);
-	~Wall();
-};
-
-class Character{
-public:
-	int sprite,numJumps;
-	int COLS,ROWS,imgx,imgy,sentido,medio;
-	float x,y,g,xSpeed,ySpeed;
-	bool moving,onGround;
-	vector<Wall*> muros;
-
-	Character(float x,float y,vector<Wall*> muros);
-	~Character();
-
-	void move(int i);
-	void jump();
-	void assignResource(int id);
-	void update();
-	bool checkCollide();
-};
-
-
-
-static vector<Wall*> muros;
-
-void render(Character *c,vector<ALLEGRO_BITMAP*> resources);
-=======
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
@@ -106,5 +56,4 @@ public:
 static vector<Wall*> muros;
 
 void render(Character *c,vector<ALLEGRO_BITMAP*> resources);
->>>>>>> 8f9686c7cd81c3114c64a04f99be8f434a2dbe83
 #endif
